b9ea7854af7caa00ce35d89102734e839712ad32
Victor Costa <victorcostadev@gmail.com>
2019-11-12 18:22:28 -0300

    config auth and user reducers, sagas and actions

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -3,26 +3,29 @@ import { takeLatest, call, put, all } from 'redux-saga/effects';
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { singInSuccess } from './actions';
+import { singInSuccess, singFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'session', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'session', {
+      email,
+      password,
+    });
+    const { token, user } = response.data;
 
-  const { token, user } = response.data;
+    if (!user.provider) {
+      console.tron.error('Usuário não é colaborador');
+      return;
+    }
 
-  if (!user.provider) {
-    console.tron.error('Usuário não é colaborador');
-    return;
+    yield put(singInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (error) {
+    yield put(singFailure());
   }
-
-  yield put(singInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SING_IN_REQUEST', signIn)]);
